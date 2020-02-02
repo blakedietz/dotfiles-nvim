cf4394defec84fbfa437d7929b5b4b2af8dfc38f
blakedietz <blakedietz@gmail.com>
2020-02-01 20:31:01 -0700

    fix: Add required folder creation and python3 support

--- plugin-configurations/deoplete.vim
+++ plugin-configurations/deoplete.vim
@@ -1,1 +1,2 @@
+let g:python3_host_prog = '/usr/local/bin/python3'
 let g:deoplete#enable_at_startup = 1
--- setup.sh
+++ setup.sh
@@ -5,4 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -rf ~/.config/nvim
 ln -s  ${DIR} ~/.config/nvim
 
+pip3.8 install pynvim
 
+if [ ! -d "$HOME/.vim/backups" ];
+then
+    mkdir -p "$HOME/.vim/backups"
+fi
+
+if [ ! -d "$HOME/.vim/swaps" ];
+then
+    mkdir -p "$HOME/.vim/swaps"
+fi
+
+if [ ! -d "$HOME/.vim/undo" ];
+then
+    mkdir -p "$HOME/.vim/undo"
+fi
