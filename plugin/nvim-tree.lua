211fa005a5ca9d905e194fb2058b134ff7c65934
Blake Dietz <blakedietz@gmail.com>
2022-08-09 19:23:13 -0600

    fix: duplicate setup call

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,13 +1,7 @@
--- examples for your init.lua
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
 require("nvim-tree").setup({
   open_on_setup = true,
   open_on_setup_file = true,
-  open_on_tab = true,
+  open_on_tab = false,
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
