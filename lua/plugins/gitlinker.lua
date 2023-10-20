423dc6ceb9349c6e7e872e2847306740c883ba94
Blake Dietz <blakedietz@gmail.com>
2023-10-19 20:31:46 -0600

    latest

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -5,6 +5,7 @@ return {
       {
         "<leader>gl",
         '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        desc = "Link to line in GitHub",
       },
     },
   },
